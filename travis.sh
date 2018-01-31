80609af2e778d7afa41a0d43acba1a60af649969
Jan Chren (rindeal) <dev.rindeal@gmail.com>
2018-01-31 23:59:31 +0100

    sf

--- travis.sh
+++ travis.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-equo install --verbose --multifetch 4 sys-apps/portage app-portage/repoman
-./foo.py --c foo.config --o /tmp/repo-ci
+# Usage: fold_start <TAG> <COMMENT>
+#
+# These statements can be nested and must be in pair with `fold_end()`.
+fold_start() {
+  echo -e "travis_fold:start:${1}\033[33;1m${2}\033[0m"
+}
+
+# Usage: fold_end <TAG>
+fold_end() {
+  echo -e "\ntravis_fold:end:${1}\r"
+}
+
+export LANG=en_US
+export LANGUAGE=${LANG}
+export LC_ALL=${LANG}.UTF-8
+env-update
+
+cd /repo
+
+fold_start "equo.install"
+equo install --verbose --multifetch 4 sys-apps/portage
+fold_end "equo.install"
+
+fold_start "foo.py"
+./foo.py --c ./foo.config --o /tmp/repo-ci
+fold_end "foo.py"
